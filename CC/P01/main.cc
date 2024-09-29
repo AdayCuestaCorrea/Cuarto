3aad2a0b3d73a95997bcd2ddb2ae208ec886b2d2
AdayCuestaCorrea <alu0101483887@ull.edu.es>
2024-09-29 16:02:52 +0100

    Añadida comprobación extra de parámetros

--- CC/P01/main.cc
+++ CC/P01/main.cc
@@ -18,9 +18,17 @@ int main(int argc, char* argv[]) {
   if (argc != 3) {
     std::cerr << "Usage: " << argv[0] << " <numberX> <numberY>" << std::endl;
     return 1;
-  }
+  } 
   int numberX = std::stoi(argv[1]);
   int numberY = std::stoi(argv[2]);
+  if (numberX < 0) {
+    std::cerr << "Error: numberX must be a non-negative integer." << std::endl;
+    return 1;
+  }
+  if (numberY < 0) {
+    std::cerr << "Error: numberY must be a non-negative integer." << std::endl;
+    return 1;
+  }
   PrimitiveRecursiveFunctions* pow = new Power(numberX, numberY);
   std::cout << "Result: " << pow->getResult() << std::endl;
   std::cout << "Function call count: " << pow->getFunctionCallCount() << std::endl;
